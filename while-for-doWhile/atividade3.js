a40f91e960e123fefc8788802823c1d636ed3ab6
mylenamelsilva <mylenamsfarias@gmail.com>
2022-06-05 20:30:43 -0300

    Respostas da atividade 3

---
+++ while-for-doWhile/atividade3.js
@@ -0,0 +1,19 @@
+// 1) Faça uma função que escreve os números de 1 a 10 em ordem crescente.
+
+function ordemCrescente() {
+    for (let numero = 1; numero <= 10; ++numero) {
+        console.log(numero)
+    }
+}
+
+ordemCrescente()
+
+// 2) Faça uma função que escreve os números de 10 a 1 em ordem decrescente
+
+function ordemDecrescente() {
+    for (let numero = 10; numero >= 1; numero--) {
+        console.log(numero)
+    }
+}
+
+ordemDecrescente()
